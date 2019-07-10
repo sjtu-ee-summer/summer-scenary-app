45e7ed0917e66a52ecf59d88731dff2c79a04c97
Stephentanse <1964305379@qq.com>
2019-07-10 15:10:28 +0800

    add image identify

---
+++ src/backend/imgidentify/src/main/java/com/example/imgidentify/ImgidentifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.imgidentify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgidentifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgidentifyApplication.class, args);
+    }
+
+}
