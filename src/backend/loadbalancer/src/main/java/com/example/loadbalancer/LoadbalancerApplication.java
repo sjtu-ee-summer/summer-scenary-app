b37324ec7cd22a02142b0cac1ff2744fd16bcb5f
kmykoh97 <kmykoh97@gmail.com>
2019-07-15 17:05:48 +0800

    initialised loadbalancer

---
+++ src/backend/loadbalancer/src/main/java/com/example/loadbalancer/LoadbalancerApplication.java
@@ -0,0 +1,13 @@
+package com.example.loadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadbalancerApplication.class, args);
+	}
+
+}
