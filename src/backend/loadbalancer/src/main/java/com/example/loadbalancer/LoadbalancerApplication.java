58afa3bfcc69c1ad4008e3f7fce0a03a686a290d
kmykoh97 <kmykoh97@gmail.com>
2019-07-16 16:29:00 +0800

    revert and update

--- src/backend/loadbalancer/src/main/java/com/example/loadbalancer/LoadbalancerApplication.java
+++ src/backend/loadbalancer/src/main/java/com/example/loadbalancer/LoadbalancerApplication.java
@@ -1,14 +1,33 @@
 package com.example.loadbalancer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
-@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
 @SpringBootApplication
+@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
 public class LoadbalancerApplication {
+	@LoadBalanced
+	@Bean
+	RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@RequestMapping("/invoke")
+	public String serverLocation() {
+		String servLoc = this.restTemplate.getForObject("http://server/translate/translate/text", String.class);
+		return servLoc;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoadbalancerApplication.class, args);
