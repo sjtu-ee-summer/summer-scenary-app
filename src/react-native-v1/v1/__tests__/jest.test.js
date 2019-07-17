0044c6961b37c0623c1dc1cd153a0974b41daa91
zydiii <15826138027@163.com>
2019-07-17 10:14:19 +0800

    react-native-v1

--- src/react-native-v1/v1/__tests__/jest.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import JestTest from '../JestTest';
-
-import renderer from 'react-test-renderer';
-
-test('renders correctly', () => {
-    const tree = renderer.create(<JestTest/>).toJSON();
-    expect(tree).toMatchSnapshot();
-});
