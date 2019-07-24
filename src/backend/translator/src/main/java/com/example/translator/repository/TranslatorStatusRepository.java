661025b035704260924af12a9d68f5218f9a288c
kmykoh97 <kmykoh97@gmail.com>
2019-07-24 10:00:35 +0800

    deleted unused projects

--- src/backend/translator/src/main/java/com/example/translator/repository/TranslatorStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.translator.repository;
-
-import com.example.translator.entity.TranslatorStatusEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TranslatorStatusRepository extends CrudRepository<TranslatorStatusEntity,Long> {
-}
