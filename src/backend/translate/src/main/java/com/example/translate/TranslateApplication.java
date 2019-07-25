8d63d220608372df9c0017635846dd36ad8e379a
kmykoh97 <kmykoh97@gmail.com>
2019-07-25 10:03:55 +0800

    added hystrix

--- src/backend/translate/src/main/java/com/example/translate/TranslateApplication.java
+++ src/backend/translate/src/main/java/com/example/translate/TranslateApplication.java
@@ -2,10 +2,12 @@ package com.example.translate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class TranslateApplication {
 
 	public static void main(String[] args) {
